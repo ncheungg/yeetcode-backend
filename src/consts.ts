1c08018a4f6993b3bfec68454f025002e65f0f8d
Nathan Cheung <nathancheung22@gmail.com>
2022-12-24 00:51:48 -0500

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
+import { PROBLEMS } from './problems';
+
 export const MAX_ROOM_SIZE = 15;
-export const MAX_PROBLEM_SIZE = 2510; // size of leetcode list
+export const MAX_PROBLEM_SIZE = PROBLEMS.length;
 export const PORT = 1234;
 export const PROBLEM_TIME = 30; // in minutes
 export const SOCKET_TIMEOUT = 30000; // in milliseconds
