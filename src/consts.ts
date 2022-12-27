edd9c419f05e5c6b56ab6a11f62f924b833c6a48
Nathan Cheung <nathancheung22@gmail.com>
2022-12-27 04:05:32 -0500

    removed HOST from contst

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,6 @@ import { PROBLEMS } from './problems';
 
 export const MAX_ROOM_SIZE = 15;
 export const MAX_PROBLEM_SIZE = PROBLEMS.length;
-export const HOST = 'localhost';
 export const PORT = 1234;
 export const PROBLEM_TIME = 30; // in minutes
 export const SOCKET_TIMEOUT = 30000; // in milliseconds
