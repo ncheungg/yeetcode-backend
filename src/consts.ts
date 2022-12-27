0e25295659dd06e776d7ae8747e93f11e3f191f8
Nathan Cheung <nathancheung22@gmail.com>
2022-12-27 03:09:24 -0500

    added env variable to PORT

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,6 @@ import { PROBLEMS } from './problems';
 export const MAX_ROOM_SIZE = 15;
 export const MAX_PROBLEM_SIZE = PROBLEMS.length;
 export const HOST = 'localhost';
-export const PORT = 1234;
+export const PORT = process.env.PORT || 1234;
 export const PROBLEM_TIME = 30; // in minutes
 export const SOCKET_TIMEOUT = 30000; // in milliseconds
